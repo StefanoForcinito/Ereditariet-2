41efaf976a4e3c37344746e7be61144a215a5b38
Stefano <stegallo1990@gmail.comgit config -lgit config --global user.email stegallo1990@gmail.com>
2023-12-04 14:37:22 +0100

    Aggiunto esercizio ereditarietà 2

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Forma forma = new Forma();
+        Rettangolo rettangolo = new Rettangolo();
+
+
+        forma.calcolaArea();
+        rettangolo.calcolaArea();
+    }
+
+}
---
+++ src/Rettangolo.java
@@ -0,0 +1,6 @@
+public class Rettangolo extends Forma {
+    @Override
+    public void calcolaArea() {
+        System.out.println("calcola l'area del rettangolo");
+    }
+}
